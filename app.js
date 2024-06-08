9355c83a473e7247d29393399dc823cd1ea9845a
Omkar Nitin Pathak <69031979+Omkar052000@users.noreply.github.com>
2024-06-09 00:00:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// Load the HTTP module
 const http = require('http');
 
-// Define a port to listen on
 const port = 3000;
 
-// Create a server that responds with "Hello World"
 const server = http.createServer((req, res) => {
   // Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
@@ -12,7 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-// Make the server listen on the specified port
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
